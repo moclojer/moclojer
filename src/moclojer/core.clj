b17e1251084dc25d3e75ea07e02e93d09c61b48a
Avelino <avelinorun@gmail.com>
2022-02-24 08:16:14 -0300

    start/server: use threads the expr through the forms
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/moclojer/core.clj
+++ src/moclojer/core.clj
@@ -26,18 +26,24 @@
                (conj @routers
                      [(get-in r [:endpoint :path])
                       (keyword (string/lower-case (get-in r [:endpoint :method] "get")))
-                      (fn [_] {:status (get-in r [:response :status])
-                               :body   (get-in r [:response :body])})
-                      ;; home-handler
+                      (handler r)
                       ;; TODO: slugify router-name -> remove replace `/`
                       :route-name (keyword (string/replace (get-in r [:endpoint :path]) "/" ""))])))
     (route/expand-routes @routers)))
 
 (defn -main [& _]
-  (http/start
-   (http/create-server
-    {::http/routes (make-router (yaml/from-file "moclojer.yml"))
-     ::http/type   :jetty
-     ::http/port   8000})))
+  "start moclojer server"
+  (println "(moclojer :start-server)")
+  (-> (http/create-server
+       {:env         :prod
+        ::http/routes (make-router
+                       (yaml/from-file (or (System/getenv "CONFIG")
+                                           "moclojer.yml")))
+        ::http/type   :jetty
+        ::http/join?  true
+        ::http/port   (or (some-> (System/getenv "PORT")
+                                  Integer/parseInt)
+                          8000)})
+      http/start))
 
 
