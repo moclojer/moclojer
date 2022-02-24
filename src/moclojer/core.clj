21ec7c42ec40c8e379909fa013b6f3cca3c61a3c
Avelino <avelinorun@gmail.com>
2022-02-24 12:38:23 -0300

    (make-router): returns (set) for setting up http routes
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/moclojer/core.clj
+++ src/moclojer/core.clj
@@ -5,20 +5,23 @@
             [yaml.core :as yaml])
 (:gen-class))
 
-(defn home-handler [_]
+(defn home-handler
   "home handler /"
+  [_]
   {:status 200
    :body   "(-> moclojer server)"})
 
-(defn handler [r]
+(defn handler
   "prepare function to receive http request (handler)"
+  [r]
   (fn [_] {:status       (get-in r [:endpoint :response :status] 200)
            :content-type (get-in r [:endpoint :response :headers :content-type]
                                  "application/json")
            :body         (get-in r [:endpoint :response :body] "{}")}))
 
-(defn make-router [config]
+(defn make-router
   "gets configuration structure and makes url routes based on it"
+  [config]
   (with-local-vars
    [routers #{["/" :get home-handler :route-name :home]}]
     (doseq [r config]
@@ -29,15 +32,17 @@
                       (handler r)
                       ;; TODO: slugify router-name -> remove replace `/`
                       :route-name (keyword (string/replace (get-in r [:endpoint :path]) "/" ""))])))
-    (route/expand-routes @routers)))
+    @routers))
 
-(defn -main [& _]
+(defn -main
   "start moclojer server"
+  [& _]
   (prn "(moclojer :start-server)")
   (-> {:env                    :prod
-       ::http/routes            (make-router
-                                 (yaml/from-file (or (System/getenv "CONFIG")
-                                                     "moclojer.yml")))
+       ::http/routes            (route/expand-routes
+                                 (make-router
+                                  (yaml/from-file (or (System/getenv "CONFIG")
+                                                      "moclojer.yml"))))
        ::http/type              :jetty
        ::http/join?             true
        ::http/container-options {:h2c? true}
