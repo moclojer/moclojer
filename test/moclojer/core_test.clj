3e19f1c3a553639b54e8cee14098b03efcf9946c
Enzzo Cavallo <souenzzo@gmail.com>
2022-02-24 22:44:47 -0300

    Support to openapi path template sytnax

--- test/moclojer/core_test.clj
+++ test/moclojer/core_test.clj
@@ -47,6 +47,11 @@
   (yaml/parse-string (slurp (io/resource "v3.0/petstore.yaml"))
                      :keywords false))
 
+(def petstore-expanded-spec
+  (yaml/parse-string (slurp (io/resource "v3.0/petstore-expanded.yaml"))
+                     :keywords false))
+
+
 (deftest hello-petstore-spec
   (let [config (moclojer/with-mocks
                  petstore-spec
@@ -60,9 +65,66 @@
                        http/default-interceptors
                        http/create-servlet
                        ::http/service-fn)]
-    (is (= [{:id   0
-             :name "caramelo"}]
-           (-> service-fn
-               (response-for :get "/pets")
-               :body
-               (json/parse-string true))))))
+    (testing
+      "Simple route"
+      (is (= [{:id   0
+               :name "caramelo"}]
+             (-> service-fn
+                 (response-for :get "/pets")
+                 :body
+                 (json/parse-string true)))))
+    (testing
+      "Not implemented route"
+      (is (= 501
+             (-> service-fn
+                 (response-for :post "/pets")
+                 :status))))))
+
+
+(deftest hello-petstore-expanded-spec
+  (let [config (moclojer/with-mocks
+                 petstore-expanded-spec
+                 {"find pet by id" {"status"  200
+                                    "body"    (json/generate-string {:id   0
+                                                                     :name "caramelo"})
+                                    "headers" {"Content-Type" "application/json"}}
+                  "addPet"         {"status" 303}
+                  "deletePet"      {"status" 202}
+                  "findPets"       {"status"  200
+                                    "body"    (json/generate-string [{:id   0
+                                                                      :name "caramelo"}])
+                                    "headers" {"Content-Type" "application/json"}}})
+
+
+        service-fn (-> {::http/routes (moclojer/make-router {::moclojer/config config})}
+                       http/default-interceptors
+                       http/create-servlet
+                       ::http/service-fn)]
+    (testing
+      "findPets route"
+      (is (= [{:id   0
+               :name "caramelo"}]
+             (-> service-fn
+                 (response-for :get "/pets")
+                 :body
+                 (json/parse-string true)))))
+    (testing
+      "addPet route"
+      (is (= 303
+             (-> service-fn
+                 (response-for :post "/pets")
+                 :status))))
+    (testing
+      "find pet by id route"
+      (is (= {:id   0
+              :name "caramelo"}
+             (-> service-fn
+                 (response-for :get "/pets/0")
+                 :body
+                 (json/parse-string true)))))
+    (testing
+      "deletePet route"
+      (is (= 202
+             (-> service-fn
+                 (response-for :delete "/pets/0")
+                 :status))))))
