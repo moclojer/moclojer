be592bf06454c8ac0cd5e9ac939b787b2e829484
Avelino <avelinorun@gmail.com>
2022-02-28 09:40:16 -0300

    post support: implement (body-params/body-params)
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- test/moclojer/core_test.clj
+++ test/moclojer/core_test.clj
@@ -22,6 +22,13 @@
           \"hello\": \"Hello, World!\"
         }"))
 
+(defn service-fn
+  [config]
+  (-> {::http/routes (moclojer/make-router {::moclojer/config config})}
+      http/default-interceptors
+      http/create-servlet
+      ::http/service-fn))
+
 (deftest make-router
   (testing "there must be two no items in the (set ...), the one registered
             in yaml and the home (/)"
@@ -32,10 +39,7 @@
 
 (deftest hello-world
   (let [config (yaml/parse-string (slurp "moclojer.yml"))
-        service-fn (-> {::http/routes (moclojer/make-router {::moclojer/config config})}
-                       http/default-interceptors
-                       http/create-servlet
-                       ::http/service-fn)]
+        service-fn (service-fn config)]
     (is (= {:hello "Hello, World!"}
            (-> service-fn
                (response-for :get "/hello-world")
@@ -44,10 +48,7 @@
 
 (deftest dyanamic-endpoint
   (let [config (yaml/parse-string (slurp "moclojer.yml"))
-        service-fn (-> {::http/routes (moclojer/make-router {::moclojer/config config})}
-                       http/default-interceptors
-                       http/create-servlet
-                       ::http/service-fn)]
+        service-fn (service-fn config)]
     (is (= {:hello "moclojer!"}
            (-> service-fn
                (response-for :get "/hello/moclojer")
@@ -56,12 +57,20 @@
 
 (deftest with-params
   (let [config (yaml/parse-string (slurp "moclojer.yml"))
-        service-fn (-> {::http/routes (moclojer/make-router {::moclojer/config config})}
-                       http/default-interceptors
-                       http/create-servlet
-                       ::http/service-fn)]
+        service-fn (service-fn config)]
     (is (= {:path-params "moclojer" :query-params "moclojer"}
            (-> service-fn
                (response-for :get "/with-params/moclojer?param1=moclojer")
+               :body
+               (json/parse-string true))))))
+
+(deftest first-post-route
+  (let [config (yaml/parse-string (slurp "moclojer.yml"))
+        service-fn (service-fn config)]
+    (is (= {:project "moclojer"}
+           (-> service-fn
+               (response-for :post "/first-post-route"
+                             :headers {"Content-Type" "application/json"}
+                             :body (json/encode {:project "moclojer"}))
                :body
                (json/parse-string true))))))
