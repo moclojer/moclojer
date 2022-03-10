2843791f4b86e6bc2a2c611a8f0e6d11d71d31dc
Enzzo Cavallo <souenzzo@gmail.com>
2022-03-10 08:15:16 -0300

    +multipart support, fixes #20

--- test/moclojer/aux/samples.clj
+++ test/moclojer/aux/samples.clj
@@ -1,5 +1,8 @@
 (ns moclojer.aux.samples
-  (:require [yaml.core :as yaml]))
+  (:require [io.pedestal.http :as http]
+            [yaml.core :as yaml]
+            [io.pedestal.http.ring-middlewares :as middlewares]
+            [ring.util.mime-type :as mime]))
 
 (def yaml-sample
   (yaml/parse-string "
@@ -18,9 +21,47 @@
         }"))
 
 (def edn-sample
-  [{:endpoint {:method :get
-               :path "/users"
-               :response {:status 200
-                          :headers {:content-type  "applicantion/json"}
-                          :body {:id 1 :name "chico"}}
+  [{:endpoint {:method      :get
+               :path        "/users"
+               :response    {:status  200
+                             :headers {:content-type "applicantion/json"}
+                             :body    {:id 1 :name "chico"}}
                :router-name :get-pet-by-id}}])
+
+
+(defonce *http-state (atom nil))
+
+(defn sample-upload-server
+  []
+  (let [routes #{["/" :get (fn [_]
+                             {:headers {"Content-Type" (mime/default-mime-types "html")}
+                              :body    (str
+                                         "<form method='post' enctype='multipart/form-data'>"
+                                         "<input type='file' name='name'>"
+                                         "<input name='tag'>"
+                                         "<input type='submit'>"
+                                         "</form>"
+                                         "")
+                              :status  200})
+                  :route-name ::html-form]
+                 ["/" :post [(middlewares/multipart-params)
+                             (fn [{:keys [multipart-params]}]
+                               (doseq [[k v] multipart-params]
+                                 (prn [(keyword k)
+                                       (or (some-> v :tempfile slurp)
+                                         v)]))
+                               {:headers {"Location" "/"}
+                                :status  303})]
+                  :route-name ::post-handler]}]
+    (swap! *http-state
+      (fn [st]
+        (some-> st http/stop)
+        (-> {::http/routes routes
+             ::http/type   :jetty
+             ::http/join?  false
+             ::http/port   8080}
+          http/default-interceptors
+          http/create-server
+          http/start)))))
+(comment
+  (sample-upload-server))
--- test/moclojer/openapi_test.clj
+++ test/moclojer/openapi_test.clj
@@ -1,11 +1,14 @@
-(ns moclojer.core-test
+(ns moclojer.openapi-test
   (:require [cheshire.core :as json]
             [clojure.java.io :as io]
             [clojure.test :refer [deftest is testing]]
             [io.pedestal.test :refer [response-for]]
             [moclojer.openapi :as openapi]
             [yaml.core :as yaml]
-            [moclojer.aux.service :refer [service-fn]]))
+            [moclojer.aux.service :refer [service-fn]]
+            [io.pedestal.http :as http]
+            [moclojer.router :as router])
+  (:import (java.io File)))
 
 (def petstore-spec
   (yaml/parse-string (slurp (io/resource "v3.0/petstore.yaml"))
@@ -26,16 +29,52 @@
      "Simple route"
       (is (= [{:id   0
                :name "caramelo"}]
-             (-> (service-fn config)
-                 (response-for :get "/pets")
-                 :body
-                 (json/parse-string true)))))
+            (-> (service-fn config)
+              (response-for :get "/pets"
+                :body
+                (json/parse-string true))))))
     (testing
      "Not implemented route"
       (is (= 501
-             (-> (service-fn config)
-                 (response-for :post "/pets")
-                 :status))))))
+            (-> (service-fn config)
+              (response-for :post "/pets"
+                :status)))))))
+
+
+(deftest multipart-form
+  (run! #(.delete ^File %)
+    (reverse (file-seq (io/file "upload-filesystem"))))
+  (let [spec (update-in petstore-expanded-spec
+               ["paths" "/pets" "post" "requestBody" "content"]
+               (fn [content]
+                 (assoc content
+                   "multipart/form-data"
+                   (val (first content)))))
+        config (openapi/with-mocks
+                 spec
+                 {"addPet" {"store"  "upload-filesystem"
+                            "status" 303}})
+        service-fn (-> {::http/routes (router/smart-router config)}
+                     http/default-interceptors
+                     http/dev-interceptors
+                     http/create-servlet
+                     ::http/service-fn)]
+    (is (= 303
+          (:status (response-for service-fn :post "/pets"
+                     :headers {"Content-Type" "multipart/form-data; boundary=XXXX"}
+                     :body (str "--XXXX\r\n"
+                             "Content-Disposition: form-data; name=\"name\"; filename=\"dog.txt\"\r\n"
+                             "Content-Type: text/plain\r\n\r\n"
+                             "newdog\r\n"
+                             "--XXXX\r\n"
+                             "Content-Disposition: form-data; name=\"tag\"\r\n\r\n"
+                             "beagle\r\n"
+                             "--XXXX--")))))
+    (is (= ["tag"
+            "dog.txt"]
+          (map #(.getName ^File %)
+            (.listFiles (first (.listFiles (io/file "upload-filesystem")))))))))
+
 
 (deftest hello-petstore-expanded-spec
   (let [config (openapi/with-mocks
