6c539aed886cf5fdc3a0a0a6967bca66e3bf8512
Avelino <avelinorun@gmail.com>
2022-09-04 10:28:58 -0300

    config: try File Not Found fixed: #53
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/moclojer/router.clj
+++ src/moclojer/router.clj
@@ -4,7 +4,8 @@
             [io.pedestal.log :as log]
             [moclojer.handler :as handler]
             [moclojer.openapi :as openapi]
-            [yaml.core :as yaml]))
+            [yaml.core :as yaml])
+  (:import (java.io FileNotFoundException)))
 
 (defn make-smart-router
   "Returns a pedestal routes"
@@ -12,14 +13,18 @@
   (-> (if mocks
         (do
           (log/info :mode "openapi")
-          (openapi/with-mocks
-            (yaml/from-file config false)
-            (yaml/from-file mocks false)))
+          (try
+            (openapi/with-mocks
+              (yaml/from-file config false)
+              (yaml/from-file mocks false))
+            (catch FileNotFoundException e (log/info :openfile (str "config file not found" e)))))
         (do
           (log/info :mode "moclojer")
-          (handler/moclojer->openapi
-           (if (string/ends-with? config ".edn")
-             (edn/read-string (str "[" (slurp config) "]"))
-             (cons handler/home-endpoint
-                   (yaml/from-file config))))))
+          (try
+            (handler/moclojer->openapi
+             (if (string/ends-with? config ".edn")
+               (edn/read-string (str "[" (slurp config) "]"))
+               (cons handler/home-endpoint
+                     (yaml/from-file config))))
+            (catch FileNotFoundException e (log/info :openfile (str "config file not found" e))))))
       openapi/generate-pedestal-route))
