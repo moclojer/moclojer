a7869ca8254d97ea3d5f9b915de096baf0ecbaa6
Avelino <avelinorun@gmail.com>
2022-10-31 09:24:11 -0300

    test/io utils: merge yaml write config
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- test/moclojer/io_utils.clj
+++ test/moclojer/io_utils.clj
@@ -1,4 +1,5 @@
 (ns moclojer.io-utils
+  (:refer-clojure :exclude [load])
   (:require [clojure.string :as string]
             [yaml.core :as yaml])
   (:import (java.nio.file Files)
@@ -6,16 +7,16 @@
            (java.io File)))
 
 (defn write-config
+  "write configuration file with receiving data type (yaml or edn) and structure"
   [extension values]
   (let [dir (.toFile (Files/createTempDirectory "moclojer"
-                                                ^"[Ljava.nio.file.attribute.FileAttribute;" (into-array FileAttribute [])))
-        file (File/createTempFile "config" (str "." extension)
-                                  dir)]
+                                                ^"[Ljava.nio.file.attribute.FileAttribute;"
+                                                (into-array FileAttribute [])))
+        file (File/createTempFile "config" (str "." extension) dir)]
     (spit file
           (case extension
             "edn" (string/join (System/getProperty "line.separator")
                                (map str values))
-            "yaml" (yaml/generate-string values)
-            "yml" (yaml/generate-string values)))
+            ("yml" "yaml") (yaml/generate-string values)))
     (.deleteOnExit file)
     (.getCanonicalPath file)))
