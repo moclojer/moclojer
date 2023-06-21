3d675d63e29113d381749405b5f0d2f53f8267df
Avelino <avelinorun@gmail.com>
2023-06-21 11:03:00 -0300

    testing how to build the routes
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/moclojer/handler.clj
+++ src/moclojer/handler.clj
@@ -1,36 +1,52 @@
 (ns moclojer.handler
-  (:require [clojure.data.json :as json]
-            [clojure.string :as string]
-            [io.pedestal.http.route :as route]))
+  (:require [selmer.parser :as selmer]
+            [cheshire.core :as cheshire]
+            [clojure.string :as string]))
 
-(defn struct-handler
+(defn body->str
+  "convert body to string, if it is edn it will be converted to json->str"
+  [body]
+  (if (string? body)
+    body
+    (-> body
+        (cheshire/generate-string))))
+
+(defn generic-handler
+  [response]
+  (fn [request]
+    (prn :fn-req request)
+    {:body    (selmer/render (body->str (:body response)) request)
+     :status  (:status response)
+     :headers (into
+               {}
+               (map (fn [[k v]]
+                      [(name k) (str v)]))
+               (:headers response))}))
+
+(defn build-route
   [host method path body status headers store]
   (let [hostname (or host "localhost")
         method-keyword (keyword (string/lower-case (or method "get")))
-        router-name (keyword (str method-keyword "-" hostname "-" path))]
+        route-name (keyword (str (name method-keyword) "-"
+                                 hostname "-"
+                                 (string/replace (string/replace path "/" "")
+                                                 ":" "--")))]
+    ;; TODO: scheme support for http/https (default to http)
     #_{:host hostname :scheme :http}
-     [path
-      method-keyword
-      {:body    (json/write-str {:body body})
-       :status  status
-       :headers (into
-                 {}
-                 (map (fn [[k v]]
-                        [(name k) (str v)]))
-                 headers)}
-      :route-name router-name])
-      ;; TODO: scheme support for http/https (default to http)
-  )
+    [path
+     method-keyword
+     (generic-handler body status headers)
+     :route-name route-name]))
 
-(defn home-endpoint
-  "initial/home endpoint URI: /"
-  []
-  (route/expand-routes
-   (struct-handler
-    nil
-    "GET"
-    "/"
-    (json/write-str {:body (str '(-> moclojer server))})
-    200
-    {}
-    nil)))
+
+;; (defn home-endpoint
+;;   "initial/home endpoint URI: /"
+;;   []
+;;   (router/build
+;;    nil
+;;    "GET"
+;;    "/"
+;;    (str '(-> moclojer server))
+;;    200
+;;    {}
+;;    nil))
