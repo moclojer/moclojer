c6b27da729cb9839085f18c53ee39d0a5b5b46de
Rodrigo Campos <rod.apd@gmail.com>
2023-07-02 18:38:45 -0300

    move stuff out of core.clj

--- src/moclojer/core.clj
+++ src/moclojer/core.clj
@@ -2,10 +2,7 @@
   (:gen-class)
   (:require
    [babashka.cli :as cli]
-   [clojure.core.async :as async]
-   [clojure.edn :as edn]
    [clojure.java.io :as io]
-   [clojure.string :as string]
    [io.pedestal.http :as http]
    [io.pedestal.http.body-params :as body-params]
    [io.pedestal.http.jetty]
@@ -13,17 +10,11 @@
    [io.pedestal.log :as log]
    [moclojer.adapters :as adapters]
    [moclojer.config :as config]
+   [moclojer.io-utils :refer [open-file]]
    [moclojer.router :as router]
-   [yaml.core :as yaml])
+   [moclojer.watcher :refer [start-watcher]])
   (:import
-   (java.io FileNotFoundException)
-   (java.nio.file
-    FileSystems
-    Path
-    StandardWatchEventKinds
-    WatchEvent)
    (java.util Properties)
-   (java.util.concurrent TimeUnit)
    (org.eclipse.jetty.server.handler.gzip GzipHandler)
    (org.eclipse.jetty.servlet ServletContextHandler)))
 
@@ -36,42 +27,6 @@
     (.setGzipHandler context gzip-handler))
   context)
 
-(defn watch-service
-  "watch spec file change to server reload
-   used async/thread to not have time for server shutdown"
-  [files on-change]
-  (let [ws (.newWatchService (FileSystems/getDefault))
-        kv-paths (keep (fn [x]
-                         (when-let [path (some-> x
-                                                 io/file
-                                                 .toPath
-                                                 .toAbsolutePath)]
-                           [x path]))
-                       files)
-        roots (into #{}
-                    (keep (fn [[_ ^Path v]]
-                            (.getParent v)))
-                    kv-paths)]
-    (doseq [path roots]
-      (.register ^Path path
-                 ws (into-array [StandardWatchEventKinds/ENTRY_MODIFY
-                                 StandardWatchEventKinds/OVERFLOW
-                                 StandardWatchEventKinds/ENTRY_DELETE
-                                 StandardWatchEventKinds/ENTRY_CREATE])))
-    (async/thread
-      (loop []
-        (when-let [watch-key (.poll ws 1 TimeUnit/SECONDS)]
-          (let [changed (keep (fn [^WatchEvent event]
-                                (let [^Path changed-path (.context event)]
-                                  (first (for [[k v] kv-paths
-                                               :when (= v (.toAbsolutePath changed-path))]
-                                           k))))
-                              (.pollEvents watch-key))]
-            (when (seq changed)
-              (on-change (set changed))))
-          (.reset watch-key))
-        (recur)))))
-
 (def *pom-info
   "pom file info load"
   (delay
@@ -81,16 +36,6 @@
               io/reader
               (->> (.load p)))
       p)))
-
-(defn open-file [path]
-  (if (empty? path)
-    (log/error :open-config "file not found")
-    (try
-      (if (string/ends-with? path ".edn")
-        (edn/read-string (str "[" (slurp path) "]"))
-        (yaml/from-file path))
-      (catch FileNotFoundException e
-        (log/error :open-config (str "file not found" e))))))
 
 (defn start
   "start moclojer server"
@@ -106,7 +51,7 @@
         *router (atom (router/smart-router
                        (open-file config)
                        (open-file mocks)))]
-    (watch-service
+    (start-watcher
      (vals envs)
      (fn [changed]
        (log/info :changed changed)
---
+++ src/moclojer/io_utils.clj
@@ -0,0 +1,17 @@
+(ns moclojer.io-utils
+  (:require
+   [clojure.edn :as edn]
+   [clojure.string :as string]
+   [io.pedestal.log :as log]
+   [yaml.core :as yaml])
+  (:import [java.io FileNotFoundException]))
+
+(defn open-file [path]
+  (if (empty? path)
+    (log/error :open-config "file not found")
+    (try
+      (if (string/ends-with? path ".edn")
+        (edn/read-string (str "[" (slurp path) "]"))
+        (yaml/from-file path))
+      (catch FileNotFoundException e
+        (log/error :open-config (str "file not found" e))))))
