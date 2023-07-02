fde9dfc31fdc938a8aa25523efb57b8e79b92856
Rodrigo Campos <rod.apd@gmail.com>
2023-07-02 18:59:16 -0300

    refactor router

--- src/moclojer/router.clj
+++ src/moclojer/router.clj
@@ -1,14 +1,18 @@
 (ns moclojer.router
   (:require
+   [io.pedestal.log :as log]
    [moclojer.routes :as routes]
-   [moclojer.specs.moclojer :as moclojer-spec]
-   [moclojer.specs.openapi :as openapi-spec]))
+   [moclojer.specs.openapi :as openapi]))
 
 (defn smart-router
   "identifies configuration type (moclojer or openapi spec)"
   [config mocks]
   (routes/generate-routes
    (if mocks
-     (openapi-spec/->moclojer config mocks)
-     (moclojer-spec/->moclojer config))))
+     (do
+       (log/info :mode "openapi")
+       (openapi/->moclojer config mocks))
+     (do
+       (log/info :mode "moclojer")
+       config))))
 
