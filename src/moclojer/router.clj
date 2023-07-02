1cbd521a6897ff718e0716d378c0a11cd14df6b9
Rodrigo Campos <rod.apd@gmail.com>
2023-07-02 17:57:05 -0300

    refactor router and remove old files

--- src/moclojer/router.clj
+++ src/moclojer/router.clj
@@ -1,13 +1,14 @@
 (ns moclojer.router
-  (:require [moclojer.specs.moclojer :as spec]
-            [moclojer.specs.openapi :as openapi]))
+  (:require
+   [moclojer.routes :as routes]
+   [moclojer.specs.moclojer :as moclojer-spec]
+   [moclojer.specs.openapi :as openapi-spec]))
 
 (defn smart-router
   "identifies configuration type (moclojer or openapi spec)"
-  [envs]
-  (let [config (:moclojer.router/config envs)
-        mocks (:moclojer.router/mocks envs)]
-    (if mocks
-      (openapi/->moclojer config mocks)
-      (spec/generate-routes-by-file config))))
+  [config mocks]
+  (routes/generate-routes
+   (if mocks
+     (openapi-spec/->moclojer config mocks)
+     (moclojer-spec/->moclojer config))))
 
