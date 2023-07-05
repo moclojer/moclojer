a017ddc21422e80c1cc83da3d509cb6e5c8642e6
Rodrigo Campos <rod.apd@gmail.com>
2023-07-05 18:59:17 -0300

    add home-endpoint again

--- src/moclojer/router.clj
+++ src/moclojer/router.clj
@@ -1,17 +1,29 @@
 (ns moclojer.router
-  (:require [io.pedestal.log :as log]
+  (:require [clojure.data.json :as json]
+            [io.pedestal.log :as log]
             [moclojer.specs.moclojer :as spec]
             [moclojer.specs.openapi :as openapi]))
+
+(def home-endpoint
+  "initial/home endpoint URL: /"
+  {:endpoint {:method :get
+              :path "/"
+              :router-name ::moclojer
+              :response {:headers {}
+                         :body (json/write-str (str '(-> moclojer server)))
+                         :status 200}}})
 
 (defn smart-router
   "identifies configuration type (moclojer or openapi spec)"
   [{:keys [::config ::mocks]}]
-  (spec/->pedestal
+  (->>
    (if mocks
      (do
        (log/info :mode "openapi")
        (openapi/->moclojer config mocks))
      (do
        (log/info :mode "moclojer")
-       config))))
+       config))
+   (cons home-endpoint)
+   (spec/->pedestal)))
 
