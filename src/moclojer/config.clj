1d3a0764a1faa889edd153bcc8d27d49d55ff462
Avelino <avelinorun@gmail.com>
2023-07-14 10:01:24 -0300

    leave core package clean (low cognitive load)
    simplify the startup package by calling internal packages with extremely defined responsibilities
    
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/moclojer/config.clj
+++ src/moclojer/config.clj
@@ -1,7 +1,23 @@
-(ns moclojer.config)
+(ns moclojer.config
+  (:require [babashka.cli :as cli]
+            [clojure.java.io :as io])
+  (:import (java.util Properties)))
+
+(def *pom-info
+  "pom file info load"
+  (delay
+    (let [p (Properties.)]
+      (some-> "META-INF/maven/moclojer/moclojer/pom.properties"
+              io/resource
+              io/reader
+              (->> (.load p)))
+      p)))
+
+(def version
+  "get version from pom properties"
+  (or (get @*pom-info "version") "dev"))
 
 ;; https://specifications.freedesktop.org/basedir-spec/latest/ar01s03.html
-
 (def get-home (System/getProperty "user.home"))
 
 (def get-xdg-config-home
@@ -12,3 +28,22 @@
 (defn with-xdg
   "Will prefix with the XDG home."
   [s] (str get-xdg-config-home "/" s))
+
+(def spec
+  "Configuration Parameters"
+  {:config {:ref     "<file>"
+            :desc    "Config path <file> or the CONFIG enviroment variable."
+            :alias   :c
+            :default "moclojer.yml"}
+   :mocks  {:ref     "<file>"
+            :desc    "OpenAPI v3 mocks path <file> or the MOCKS enviroment variable."
+            :alias   :m}
+   :version {:desc   "Show version."
+             :alias  :v}
+   :help    {:desc   "Show this Help."
+             :alias  :h}})
+
+(def empty-args
+  "Args are empty"
+  (str "moclojer (" version "), simple and efficient HTTP mock server.\r\n"
+       (cli/format-opts {:spec spec :order [:config :mocks :version :help]})))
---
+++ src/moclojer/server.clj
@@ -0,0 +1,68 @@
+(ns moclojer.server
+  (:require [io.pedestal.http :as http]
+            [io.pedestal.http.body-params :as body-params]
+            [io.pedestal.http.jetty]
+            [io.pedestal.log :as log]
+            [moclojer.server :as server]
+            [moclojer.io-utils :refer [open-file]]
+            [moclojer.router :as router]
+            [moclojer.watcher :refer [start-watcher]])
+  (:import (org.eclipse.jetty.server.handler.gzip GzipHandler)
+           (org.eclipse.jetty.servlet ServletContextHandler)))
+
+(defn context-configurator
+  "http container options, active gzip"
+  [^ServletContextHandler context]
+  (let [gzip-handler (GzipHandler.)]
+    (.addIncludedMethods gzip-handler (make-array String 0))
+    (.setExcludedAgentPatterns gzip-handler (make-array String 0))
+    (.setGzipHandler context gzip-handler))
+  context)
+
+(defn get-interceptors [service-map]
+  (-> service-map
+      http/default-interceptors
+      (update ::http/interceptors into [http/json-body
+                                        (body-params/body-params)])))
+
+(defn start
+  "start moclojer server"
+  [{:keys [current-version config-path mocks-path]}]
+
+  (let [generate-routes (fn [config-path mocks-path]
+                          (router/smart-router {::router/config (open-file config-path)
+                                                ::router/mocks  (open-file mocks-path)}))
+        http-host (or (System/getenv "HOST") "0.0.0.0")
+        http-port (or (some-> (System/getenv "PORT")
+                              Integer/parseInt)
+                      8000)
+        *router (atom (generate-routes config-path mocks-path))
+        get-routes (fn [] @*router)]
+    (log/info
+     "-> moclojer"
+     :start-server
+     :host http-host
+     :port http-port
+     :url (str "http://" http-host ":" http-port)
+     :version current-version
+     :config-path config-path
+     :mocks-path mocks-path)
+    (start-watcher
+     [config-path mocks-path]
+     (fn [changed]
+       (log/info :changed changed)
+       (reset! *router (generate-routes config-path mocks-path))))
+    (-> {:env                     :prod
+         ::http/routes            get-routes
+         ::http/type              :jetty
+         ::http/join?             true
+         ;; pedestal default behavior is to return 403 for invalid origins and
+         ;; return Access-Control-Allow-Origin as nil
+         ::http/allowed-origins   {:creds true :allowed-origins (constantly true)}
+         ::http/container-options {:h2c?                 true
+                                   :context-configurator context-configurator}
+         ::http/host              http-host
+         ::http/port              http-port}
+        get-interceptors
+        http/create-server
+        http/start)))
