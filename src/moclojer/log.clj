e51e97a6660917c42e8edc733ff055307b504a1b
Avelino <avelinorun@gmail.com>
2023-07-19 13:42:17 -0300

    moclojer/logs: allow log of moclojer.* (internal packages)
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/moclojer/log.clj
+++ src/moclojer/log.clj
@@ -3,11 +3,12 @@
             [taoensso.timbre.appenders.core :as core-appenders]))
 
 (defn setup [level stream]
-  (timbre/set-min-level! level)
   (timbre/merge-config!
-   {:appenders
-    {:println
-     (core-appenders/println-appender {:stream stream})}}))
+   {:min-level level
+    :ns-filter {:deny #{"*"}
+                :allow #{"moclojer.*"}}
+    :appenders
+    {:println (core-appenders/println-appender {:stream stream})}}))
 
 (defmacro log [level & args]
   `(timbre/log! ~level :p ~args ~{:?line (:line (meta &form))}))
