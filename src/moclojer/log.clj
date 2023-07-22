3ad5c6b32e5216d030220b28abe2e35899bd24bb
Avelino <avelinorun@gmail.com>
2023-07-22 11:22:17 -0300

    java.util.logging: use to disable jetty log we will keep the timbre as written log standard we know it will have more "layers", at this point we need simplicity
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/moclojer/log.clj
+++ src/moclojer/log.clj
@@ -1,14 +1,39 @@
 (ns moclojer.log
-  (:require [taoensso.timbre :as timbre]
-            [taoensso.timbre.appenders.core :as core-appenders]))
+  (:require [clojure.string :as string]
+            [taoensso.timbre :as timbre]
+            [taoensso.timbre.appenders.core :as core-appenders])
+  (:import (java.util.logging Filter Formatter Handler LogRecord Logger)))
 
-(defn setup [level stream]
+(set! *warn-on-reflection* true)
+
+(defn global-setup
+  "global setup for logging, clear `org.eclipse.jetty` logs"
+  [^Logger logger]
+  (doseq [^Handler handler (.getHandlers logger)]
+    (.setFormatter
+     handler
+     (proxy [Formatter] []
+       (format [^LogRecord log-record]
+         (str (.getLevel log-record) " [" (.getLoggerName log-record) "] - "
+              (.formatMessage ^Formatter this log-record) "\n"))))
+    (.setFilter
+     handler
+     (reify Filter
+       (isLoggable [this record]
+         (not (string/starts-with? (.getLoggerName record)
+                                   "org.eclipse.jetty")))))))
+
+(defn setup
+  "timbre setup for logging"
+  [level stream]
+  (global-setup (.getParent (Logger/getGlobal))) ;; disable `org.eclipse.jetty` logs
   (timbre/merge-config!
    {:min-level level
-    :ns-filter {:deny #{"*"}
-                :allow #{"moclojer.*"}}
+    :ns-filter {:allow #{"moclojer.*"}}
     :appenders
     {:println (core-appenders/println-appender {:stream stream})}}))
 
-(defmacro log [level & args]
+(defmacro log
+  "log macro for logging with timbre"
+  [level & args]
   `(timbre/log! ~level :p ~args ~{:?line (:line (meta &form))}))
