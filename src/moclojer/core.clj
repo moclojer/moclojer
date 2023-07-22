ee6cf602c725e078f1e3989416b8368cedb04029
Avelino <avelinorun@gmail.com>
2023-07-22 11:22:17 -0300

    java.util.logging: use to disable jetty log we will keep the timbre as written log standard we know it will have more "layers", at this point we need simplicity
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/moclojer/core.clj
+++ src/moclojer/core.clj
@@ -2,39 +2,18 @@
   (:gen-class)
   (:require [babashka.cli :as cli]
             [clojure.string :as string]
-            [io.pedestal.log :as log]
             [moclojer.adapters :as adapters]
             [moclojer.config :as config]
-            [moclojer.log :as logs]
+            [moclojer.log :as log]
             [moclojer.server :as server])
   (:import (java.util.logging Filter Formatter Handler LogRecord Logger)))
 
-(set! *warn-on-reflection* true)
-
-(defn setup-logger
-  [^Logger logger]
-  (doseq [^Handler handler (.getHandlers logger)]
-    (.setFormatter handler (proxy [Formatter] []
-                             (format [^LogRecord log-record]
-                               (str
-                                 (.getLevel log-record)
-                                 " ["
-                                 (.getLoggerName log-record)
-                                 "] - "
-                                 (.formatMessage ^Formatter this log-record)
-                                 "\n"))))
-
-    (.setFilter handler
-      (reify Filter (isLoggable [this record]
-                      (not (string/starts-with? (.getLoggerName record)
-                             "org.eclipse.jetty")))))))
 
 (defn -main
   "software entry point"
   {:org.babashka/cli {:collect {:args []}}}
   [& args]
-  (setup-logger (.getParent (Logger/getGlobal)))
-  ;; (logs/setup :info :auto)
+  (log/setup :info :auto)
   (let [args-opts (cli/parse-args args {:spec config/spec})
         envs {:config (or (System/getenv "CONFIG")
                           (config/with-xdg "moclojer.yml"))
