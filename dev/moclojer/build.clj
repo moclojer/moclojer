e454aeb86f564fab1cb42d7c6a7b394712253d29
Avelino <avelinorun@gmail.com>
2023-10-16 09:20:21 -0300

    clj:build: reorganize the process and native image
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- dev/moclojer/build.clj
+++ dev/moclojer/build.clj
@@ -11,31 +11,52 @@
 
 (set! *warn-on-reflection* true)
 
+(defmacro with-err-str
+  [& body]
+  `(let [s# (new java.io.StringWriter)]
+     (binding [*err* s#]
+       ~@body
+       (str s#))))
+
 (defn -main
   [& _]
   (let [basis (b/create-basis {:project "deps.edn"})]
+    (println "Clearing target directory")
     (b/delete {:path "target"})
-    (b/write-pom {:class-dir class-dir
-                  :lib       'moclojer/moclojer
-                  :version   config/version
-                  :basis     basis
-                  :src-dirs  (:paths basis)})
-    (b/compile-clj {:basis     basis
-                    :src-dirs  (:paths basis)
-                    :class-dir class-dir})
+
+    (println "Writing pom")
+    (->> (b/write-pom {:class-dir class-dir
+                       :lib       'moclojer/moclojer
+                       :version   config/version
+                       :basis     basis
+                       :src-dirs  (:paths basis)})
+         with-err-str
+         string/split-lines
+         ;; Avoid confusing future me/you: suppress "Skipping coordinate" messages for our jars, we don't care, we are creating an uberjar
+         (remove #(re-matches #"^Skipping coordinate: \{:local/root .*target/(lib1|lib2|graal-build-time).jar.*" %))
+         (run! println))
+    (b/copy-dir {:src-dirs (:paths basis)
+                 :target-dir class-dir})
+
+    (println "Compile sources to classes")
+    (b/compile-clj {:basis      basis
+                    :src-dirs   (:paths basis)
+                    :class-dir  class-dir
+                    :ns-compile '[moclojer.core]})
+
+    (println "Building uberjar")
     (b/uber {:class-dir class-dir
              :main      'moclojer.core
              :uber-file uber-file
              :basis     basis})
 
-    (.mkdirs (io/file "target" "native"))
-
+    (println "Building native image configuration files")
     ;; create native-image configuration file `filter.json`
-    (spit (io/file "target" "native" "filter.json")
+    (spit (io/file "target" "filter.json")
           (json/write-str {:rules []}))
 
     ;; create native-image parameter file `@native-image-args`
-    (spit (io/file "target" "native" "native-image-args")
+    (spit (io/file "target" "native-image-args")
           (string/join "\n" ["-H:Name=moclojer"
                              "-Dio.pedestal.log.defaultMetricsRecorder=nil"
                              "-Dorg.slf4j.simpleLogger.defaultLogLevel=error"
@@ -44,14 +65,13 @@
 
                              ;; TODO: Option 'EnableAllSecurityServices' is deprecated
                              "--enable-all-security-services"
-                             "--features=clj_easy.graal_build_time.InitClojureClasses"
+
+                             ;; TODO: use clj-easy.graal-build-time
+                             ;; "--features=clj_easy.graal_build_time.InitClojureClasses"
 
                              ;; lists managed in the native-image library
                              native-image/initialize-at-build-time
-                             native-image/initialize-at-run-time
-                             native-image/trace-class-initialization
 
-                             ;; "-H:+PrintClassInitialization"
                              "-H:DashboardDump=report/moclojer"
                              "-H:+ReportExceptionStackTraces"
                              "-H:+DashboardHeap"
