6c5ad0a7f9cb7e850ee3a849125c794a49e11205
Avelino <hey@avelino.run>
2023-11-12 11:28:57 -0300

    write unit test for function request-after-delay
    Signed-off-by: Avelino <hey@avelino.run>

--- test/moclojer/webhook_test.clj
+++ test/moclojer/webhook_test.clj
@@ -4,7 +4,10 @@
             [io.pedestal.http :as http]
             [io.pedestal.test :refer [response-for]]
             [moclojer.router :as router]
+            [moclojer.webhook :as webhook]
             [yaml.core :as yaml]))
+
+(def body {:id 123})
 
 (deftest server-with-webhook
   (let [service-fn (-> {::http/routes (router/smart-router
@@ -13,8 +16,26 @@
                        http/dev-interceptors
                        http/create-servlet
                        ::http/service-fn)]
-    (is (= {:id 123}
+    (is (= body
            (-> service-fn
-               (response-for :get "/with-webhook")
+               (response-for :post "/with-webhook")
                :body
+               (json/parse-string true))))))
+
+(deftest call-request-after-delay
+  (do
+    ;; upload the server to test on the internal endpoint
+    ;; without the need for an external request
+    (-> {::http/routes (router/smart-router
+                        {::router/config (yaml/from-file "test/moclojer/resources/webhook.yml")})
+         ::http/port 8000}
+        http/default-interceptors
+        http/dev-interceptors
+        http/create-servlet
+        ::http/service-fn)
+    (is (= body
+           (-> (webhook/request-after-delay
+                "http://127.0.0.1:8000/with-webhook"
+                :post
+                (json/generate-string body))
                (json/parse-string true))))))
