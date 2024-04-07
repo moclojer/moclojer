038035398c1675cec341b96133ca34c39f53a20c
Avelino <31996+avelino@users.noreply.github.com>
2024-04-07 18:06:40 -0300

    Validate condition before triggering the webhook request
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/com/moclojer/webhook.clj
+++ src/com/moclojer/webhook.clj
@@ -5,19 +5,21 @@
 
 (defn request-after-delay
   "after a delay call http-request, return body"
-  [{:keys [url method body headers sleep-time]
+  [{:keys [url method body headers sleep-time condition]
     :or {headers {}
+         condition true
          ; in milliseconds, 1 minute is 60000 milliseconds
          sleep-time 60}}]
   (let [req {:url url
              :method method
              :headers headers
              :body body}]
-    (a/go
-      (a/thread
-        (do
-          (log/log :info :sleep sleep-time :webhook-start req)
-          (Thread/sleep (long sleep-time))
-          (client/request req)
-          (log/log :info :sleep sleep-time :webhook-done req))))
+    (if condition
+      (a/go
+        (a/thread
+          (do
+            (log/log :info :sleep sleep-time :webhook-start req)
+            (Thread/sleep (long sleep-time))
+            (client/request req)
+            (log/log :info :sleep sleep-time :webhook-done req)))))
     body))
