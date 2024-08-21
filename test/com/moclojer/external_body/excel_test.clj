bdfdefdf58281e07e2ef2edbfffcceb5feed9c8f
J0sueTM <teodoro.josue@protonmail.ch>
2024-08-21 13:53:59 -0300

    fix: use real path instead of file again...

--- test/com/moclojer/external_body/excel_test.clj
+++ test/com/moclojer/external_body/excel_test.clj
@@ -1,14 +1,14 @@
 (ns com.moclojer.external-body.excel-test
-  (:require [cheshire.core :as json]
-            [clojure.test :refer [deftest is]]
-            [com.moclojer.helpers-test :as helpers]
-            [io.pedestal.test :refer [response-for]]
-            [yaml.core :as yaml]))
+  (:require
+   [cheshire.core :as json]
+   [clojure.test :refer [deftest is]]
+   [com.moclojer.helpers-test :as helpers]
+   [io.pedestal.test :refer [response-for]]))
 
 (deftest xlsx-config-test
   (is (= [{:name "avelino", :langs "golang"}
           {:name "chicao", :langs "python"}]
-         (-> (helpers/service-fn (yaml/from-file "test/com/moclojer/resources/xlsx.yml")
+         (-> (helpers/service-fn "test/com/moclojer/resources/xlsx.yml"
                                  {:start? false :join? false})
              (response-for :get "/xlsx")
              :body
